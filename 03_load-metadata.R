7e1f1915e9219f7a6057b6f465945d31c531e62b
Petr Bouchal <pbouchal@gmail.com>
2020-07-29 14:39:09 +0200

    remove czso fn which was moved into package

--- 03_load-metadata.R
+++ 03_load-metadata.R
@@ -5,17 +5,6 @@ library(czso)
 cz <- load_RUIAN_state("obce")
 write_parquet(cz, here::here("data-processed", "cz_geo_meta.parquet"))
 
-library(czso)
-
-czso_get_codelist <- function(dataset_id, resource_num = 1) {
-  czso_meta <- czso:::get_czso_resource_pointer(dataset_id, resource_num)
-  cis_url <- czso_meta$url
-  cis_url_new <- str_replace(cis_url, "format\\=0$", "format=2&separator=,")
-  dt <- read_csv(cis_url_new, locale = locale(encoding = "Windows-1250"),
-                 col_types = cols(.default = "c"))
-  return(dt)
-}
-
 orp_csu <- czso_get_codelist("cis65")
 okres_csu <- czso_get_codelist("cis109")
 kraj_csu <- czso_get_codelist("cis100")
