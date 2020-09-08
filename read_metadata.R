8f0d80a6b9b1888f8e48821505b8159e1bfeb091
Petr Bouchal <pbouchal@gmail.com>
2020-09-08 02:18:03 +0200

    move metadata loading into separate script

--- read_metadata.R
+++ read_metadata.R
@@ -6,3 +6,20 @@ csu_zuj <- read_parquet(here::here("data-processed", "zuj-csu.parquet"))
 csu_obec <- read_parquet(here::here("data-processed", "obec-csu.parquet"))
 obecnuts <- read_parquet(here::here("data-processed", "obec-nuts-csu.parquet"))
 zujnuts <- read_parquet(here::here("data-processed", "zuj-nuts-csu.parquet"))
+
+ops <- read_parquet(here::here("data-processed", "op-codes.parquet"))
+vyzvy <- read_parquet(here::here("data-processed", "vyzvy-codes.parquet"))
+
+ids_and_names <- read_parquet(here::here("data-processed",
+                                         "czso-ids-all.parquet"))
+ids <- ids_and_names %>%
+  select(ends_with("_id")) %>%
+  rename_with(.fn = str_remove, pattern = "_id") %>%
+  mutate(zuj =  paste0(kraj, zuj),
+         obec = paste0(kraj, obec))
+
+ids_short <- ids_and_names %>%
+  select(ends_with("_id")) %>%
+  rename_with(.fn = str_remove, pattern = "_id")
+
+mas_all <- read_parquet(here::here("data-processed", "mas-metadata.parquet"))
