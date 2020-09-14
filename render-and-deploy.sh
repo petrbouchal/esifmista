c0e8782d42675c4633c1203909a630531d46df4d
Petr Bouchal <pbouchal@gmail.com>
2020-09-14 23:44:28 +0200

    remove unneeded shell line for PDF/DOCX codebook generation

--- render-and-deploy.sh
+++ render-and-deploy.sh
@@ -3,7 +3,6 @@
 ulimit -s 32768
 
 if
-  Rscript -e "rmarkdown::render('codebook.Rmd', output_format=c('word_document', 'pdf_document'))"
   Rscript -e "rmarkdown::render_site()";
 then
   echo "Deploying"
