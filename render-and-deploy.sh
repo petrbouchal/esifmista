6ffbf8e7be421d22d86b9c1be51fbcbae12369e9
Petr Bouchal <pbouchal@gmail.com>
2020-09-14 23:01:08 +0200

    render codebook PDF/DOCX first; they will be moved by render_site()

--- render-and-deploy.sh
+++ render-and-deploy.sh
@@ -2,9 +2,9 @@
 
 ulimit -s 32768
 
-if 
-  Rscript -e "rmarkdown::render_site()"
-  Rscript -e "rmarkdown::render('codebook.Rmd', output_format=c('word_document', 'pdf_document'))";
+if
+  Rscript -e "rmarkdown::render('codebook.Rmd', output_format=c('word_document', 'pdf_document'))"
+  Rscript -e "rmarkdown::render_site()";
 then
   echo "Deploying"
   netlify deploy --dir="docs" --prod;
