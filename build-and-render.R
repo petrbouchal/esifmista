b1bbde34101f7d88f00283982bac6b36a3d78136
Petr Bouchal <pbouchal@gmail.com>
2021-01-14 03:31:35 +0100

    cleanup run script

--- build-and-render.R
+++ build-and-render.R
@@ -8,6 +8,3 @@ source(here::here("03_load-metadata.R"))
 source(here::here("04_load-geodata.R"))
 
 rmarkdown::render_site(quiet = T)
-
-source(here::here("export_samples.R"))
-source(here::here("export_pro_vyzvu.R"))
