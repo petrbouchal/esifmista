e929c660a6f3998736129143c25521ab3acd6958
Petr Bouchal <pbouchal@gmail.com>
2021-01-31 01:16:02 +0100

    cleanup excel export script

--- export-excel.R
+++ export-excel.R
@@ -15,25 +15,13 @@ dir <- cnf$excel_output_dir
 dir.create(dir, showWarnings = F, recursive = T)
 
 save_one_excel <- function(ds, op, chnk, dir) {
-  # print(op)
-  # print(chnk)
   df_op_chunk <- ds %>%
     filter(chunk == chnk, op_id == op) %>%
     collect()
-
-  # print(df_op_chunk)
-
   chunk_padded <- str_pad(chnk, width = 2, pad = "0")
   filename <- file.path(dir, paste0(op, "_", chunk_padded, ".xlsx"))
   write_xlsx(df_op_chunk, filename)
 }
 
-save_one_excel(ds = ds, op = "IROP", chnk = 1, dir = dir)
 walk2(ops_chunks$op_id, ops_chunks$chunk, ~save_one_excel(ds, .x, .y, dir))
-
-ds %>%
-  filter(op_id == "OP PPR", chunk == 1) %>%
-  collect()
-
-ds
 
